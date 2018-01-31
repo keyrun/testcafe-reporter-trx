89875c3579a86356fbab37974d64e47a437c48af
Kiran Paul <kipaul@microsoft.com>
2018-01-30 19:05:16 -0800

    Initial changes to support trx format

--- src/index.js
+++ src/index.js
@@ -1,38 +1,99 @@
+const TRX = require('node-trx');
+const os = require('os');
+
+const computerName = os.hostname();
+const userName = os.userInfo().username;
+const TestRun = TRX.TestRun;
+const UnitTest = TRX.UnitTest;
+const newLine = os.EOL;
+
 export default function () {
     return {
         noColors: true,
         
-        createErrorDecorator () {
-            return {
-                'span category':       () => '',
-                'span step-name':      str => `"${str}"`,
-                'span user-agent':     str => this.chalk.gray(str),
-                'div screenshot-info': str => str,
-                'a screenshot-path':   str => this.chalk.underline(str),
-                'code':                str => this.chalk.yellow(str),
-                'code step-source':    str => this.chalk.magenta(this.indentString(str, 4)),
-                'span code-line':      str => `${str}\n`,
-                'span last-code-line': str => str,
-                'code api':            str => this.chalk.yellow(str),
-                'strong':              str => this.chalk.cyan(str),
-                'a':                   str => this.chalk.yellow(`"${str}"`)
-            };
-        },
-        
-        reportTaskStart (/* startTime, userAgents, testCount */) {
-            throw new Error('Not implemented');
+        reportTaskStart (startTime/*, userAgents, testCount*/) {
+            this.startTime = startTime;
+            this.tests = [];
         },
 
-        reportFixtureStart (/* name, path */) {
-            throw new Error('Not implemented');
+        reportFixtureStart (name, path) {
+            this.fixtureName = name;
+            this.path = path;
         },
 
-        reportTestDone (/* name, testRunInfo */) {
-            throw new Error('Not implemented');
+        reportTestDone (name, testRunInfo) {
+            const end = new Date(+this.startTime + testRunInfo.durationMs);
+            const hasErr = !!testRunInfo.errs.length;
+            let result = hasErr ? `Failed` : `Passed`;
+
+            result = testRunInfo.skipped ? 'Skipped' : result;
+            const testResults = {};
+
+            testResults.name = `test "${name}" in fixture "${this.fixtureName}"`;
+            if (hasErr)
+                testResults.errs = testRunInfo.errs;
+            testResults.outcome = result;
+            testResults.duration = testRunInfo.durationMs;
+            testResults.start = this.testStartTime;
+            testResults.end = end;
+            this.tests.push(testResults);
         },
 
-        reportTaskDone (/* endTime, passed, warnings */) {
-            throw new Error('Not implemented');
+        reportTaskDone (endTime/*, passed, warnings*/) {
+            const now = endTime.toISOString();
+            const testRunName = userName + '@' + computerName + ' ' + now.substring(0, now.indexOf('.')).replace('T', ' ');
+            const run = new TestRun({
+                name:     testRunName,
+                runUser:  userName,
+                settings: {
+                    name: 'default'
+                },
+                times: {
+                    creation: now,
+                    queuing:  now,
+                    start:    this.startTime.toISOString(),
+                    finish:   endTime.toISOString()
+                }
+            });
+            
+            this.tests.map((test) => {
+                const unittest = new UnitTest({
+                    name:            test.name,
+                    methodCodeBase:  'none',
+                    methodName:      'none',
+                    methodClassName: 'none'
+                });
+                const errorMessage    = test.errs ? `${test.name} encountered errors` : '';
+                let errorStacktrace = '';
+
+                if (test.errs) {
+                    test.errs.map((err, idx) => {
+                        const prefix = this.chalk.red(`${idx + 1}) `);
+
+                        errorStacktrace += this.formatError(err, prefix) + newLine;
+                    });
+                }
+
+                run.addResult({
+                    test:            unittest,
+                    computerName:    computerName,
+                    outcome:         test.outcome,
+                    duration:        formatDuration(test.duration || 0),
+                    startTime:       test.start && test.start.toISOString() || '',
+                    endTime:         test.end && test.end.toISOString() || '',
+                    errorMessage:    errorMessage,
+                    errorStacktrace: errorStacktrace
+                });
+            });
+            this.write(run.toXml());
         }
     };
+}
+
+function formatDuration (milliseconds) {
+    // we get duration ISO string
+    var duration = (new Date(milliseconds)).toISOString();
+
+    // we return time part only and remove Z char
+    return duration.substring(duration.indexOf('T') + 1).replace('Z', '');
 }
--- test/test.js
+++ test/test.js
@@ -2,13 +2,14 @@ var assert           = require('assert');
 var normalizeNewline = require('normalize-newline');
 var read             = require('read-file-relative').readSync;
 var createReport     = require('./utils/create-report');
+var regex            = /((id)|(relativeResultsDirectory))="([\w-]+)"/gim;
 
 it('Should produce report with colors', function () {
     var report   = createReport(true);
     var expected = JSON.parse(read('./data/report-with-colors.json'));
 
-    report   = normalizeNewline(report).trim();
-    expected = normalizeNewline(expected).trim();
+    report   = normalizeNewline(report).trim().replace(regex, '$1=""');
+    expected = normalizeNewline(expected).trim().replace(regex, '$1=""');
 
     assert.strictEqual(report, expected);
 });
@@ -17,8 +18,8 @@ it('Should produce report without colors', function () {
     var report   = createReport(false);
     var expected = read('./data/report-without-colors');
 
-    report   = normalizeNewline(report).trim();
-    expected = normalizeNewline(expected).trim();
+    report   = normalizeNewline(report).trim().replace(regex, '$1=""');
+    expected = normalizeNewline(expected).trim().replace(regex, '$1=""');
 
     assert.strictEqual(report, expected);
 });
